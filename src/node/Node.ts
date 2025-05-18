6b3bec8bcd075422e460fa11afd616a8bf3e0baa
nik9 <n-bogun@mail.ru>
2025-05-18 16:12:33 +0300

    better penalties calculations

--- src/node/Node.ts
+++ src/node/Node.ts
@@ -188,8 +188,8 @@ export class Node extends TypedEventEmitter<NodeEvents> {
 		penalties += Math.round(Math.pow(1.05, 100 * this.stats.cpu.systemLoad) * 10 - 10);
 
 		if (this.stats.frameStats) {
-			penalties += this.stats.frameStats.deficit;
-			penalties += this.stats.frameStats.nulled * 2;
+			penalties += Math.pow(1.03, 500 * Math.abs(this.stats.frameStats.deficit / 3000)) * 600 - 600;
+			penalties += Math.pow(1.03, 500 * Math.abs(this.stats.frameStats.nulled / 3000)) * 300 - 300;
 		}
 
 		return penalties;
