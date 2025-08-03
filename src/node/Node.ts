a0fc0c25445fa1eccf25c9ce49f4ce5ddda01074
nik9 <n-bogun@mail.ru>
2025-08-03 22:01:30 +0300

    Revert "change penalties calculations"
    This reverts commit 9b418ef4ae08b1fc8fa4c71d5915e3a66ae68904.

--- src/node/Node.ts
+++ src/node/Node.ts
@@ -184,8 +184,8 @@ export class Node extends TypedEventEmitter<NodeEvents> {
 		let penalties = 0;
 		if (!this.stats) return penalties;
 
-		penalties += this.stats.playingPlayers;
-		penalties += Math.round(Math.pow(1.05, 100 * this.stats.cpu.systemLoad) * 100 - 100);
+		penalties += this.stats.players;
+		penalties += Math.round(Math.pow(1.05, 100 * this.stats.cpu.systemLoad) * 10 - 10);
 
 		if (this.stats.frameStats) {
 			penalties += this.stats.frameStats.deficit;
