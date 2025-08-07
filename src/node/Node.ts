d3d38be19f9d5d9c4d0eaefe5148d7bcb05099cb
nik9 <n-bogun@mail.ru>
2025-08-07 11:35:45 +0300

    use playingplayers for penalties

--- src/node/Node.ts
+++ src/node/Node.ts
@@ -184,7 +184,7 @@ export class Node extends TypedEventEmitter<NodeEvents> {
 		let penalties = 0;
 		if (!this.stats) return penalties;
 
-		penalties += this.stats.players;
+		penalties += this.stats.playingPlayers;
 		penalties += Math.round(Math.pow(1.05, 100 * this.stats.cpu.systemLoad) * 10 - 10);
 
 		if (this.stats.frameStats) {
